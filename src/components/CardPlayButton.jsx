5e277e5d50ffabf40b4c07c5ccfe4b2394da392f
PazitoPazos <javier.pazos001@gmail.com>
2023-11-18 23:57:06 +0100

    Added musics table

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Play, Pause } from './Player'
 import { usePlayerStore } from '@/store/playerStore'
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { isPlaying, currentMusic, setIsPlaying, setCurrentMusic} = usePlayerStore(state => state)
   
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
@@ -22,10 +22,11 @@ export function CardPlayButton({ id }) {
       })
   }
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
 
   return (
-    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4'>
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'>
+      {isPlayingPlaylist ? <Pause className={iconClassName}  /> : <Play className={iconClassName} />}
     </button>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,15 +2,15 @@ import { usePlayerStore } from '@/store/playerStore'
 import { useEffect, useRef, useState } from 'react'
 import { Slider } from '@/components/Slider'
 
-export const Pause = () => (
-  <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="24" width="24" className="h-4 w-4">
+export const Pause = ({ className = 'w-4 h-4' }) => (
+  <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="24" width="24" className={className}>
   <path
     d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
   </svg>
 )
 
-export const Play = () => (
-  <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="24" width="24" className="h-4 w-4">
+export const Play = ({ className = 'w-4 h-4' }) => (
+  <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="24" width="24" className={className}>
     <path
     d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
   </svg>
